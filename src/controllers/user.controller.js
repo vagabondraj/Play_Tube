f6f289b1dc083b8ab4b31dbd9bba1bd92d59ba8b
vagabondraj <raviraj06112005@gmail.com>
2026-01-14 20:18:43 +0530

    Register Controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,44 @@
 import asyncHandler from "express-async-handler";
+import ApiError from "../utils/ApiError.js";
+import User from "../models/user.models.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message : "ok"
-    })
+    const{username, email, password, fullName} = req.body;
+    console.log("username", username);
+    
+    if(!username || !email || !password || !fullName){
+        throw new ApiError(400, "All fields are required");
+    }
+    // to validate email format
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(!emailRegex.test(email)){
+        throw new ApiError(400, "Invalid email format");
+    }
+
+    const existingUser = await User.findOne({
+        $or: [{email}, {username}]
+    });
+    if(existingUser){
+        throw new ApiError(409, "User with this email already exists");
+    }
+
+    const avatarLocalPath = req.files?.avatar[0]?.path;
+    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    if(!avatarLocalPath){
+        throw new ApiError(400, "Avatar and Cover Image are required");
+    }
+
+    const avatar = await uploadOnCloudinary(avatarLocalPath);
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+    if(!avatar){
+        throw new ApiError(500, "Failed to upload avatar");
+    }
+
+    
 });
 
 export {registerUser};
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,19 @@
 import {Router} from 'express';
 import {registerUser} from '../controllers/user.controller.js';
+import {upload} from '../middlewares/multer.middleware.js';
 
 const router = Router();
 
-router.route("/register").post(registerUser);
-
+router.route("/register").post(
+    upload.field([
+        {
+        name : "avatar",
+        maxCount: 1
+        },
+        {
+            name : "coverImage",
+            maxCount: 1
+        }
+    ]),
+);
 export default router;
