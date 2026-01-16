359dd9eff6902351b6d830f740baf18b581be239
vagabondraj <raviraj06112005@gmail.com>
2026-01-16 12:38:34 +0530

    CoverImage code correction

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -7,7 +7,6 @@ import ApiResponse from "../utils/ApiResponse..js";
 
 const registerUser = asyncHandler(async (req, res) => {
     const{username, email, password, fullname} = req.body;
-    console.log("username", username);
     
     if(!username || !email || !password || !fullname){
         throw new ApiError(400, "All fields are required");
@@ -26,7 +25,12 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    // const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath = "";
+    if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar and Cover Image are required");
@@ -61,3 +65,5 @@ const registerUser = asyncHandler(async (req, res) => {
 });
 
 export {registerUser};
+
+
