a841a0f021b63afce6b3cb43a769bf9d517d71ae
vagabondraj <raviraj06112005@gmail.com>
2026-01-17 16:57:59 +0530

    Subscription Model added

---
+++ src/models/subscription.models.js
@@ -0,0 +1,16 @@
+import mongoose, {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+    subscriber: {
+        type: Schema.Types.ObjectId,
+        ref: "user"
+    },
+    channel: {
+        type: Schema.Types.ObjectId,
+        ref: "user" 
+    }
+}, {timestamps: true});
+
+
+
+const Subscription = mongoose.model("subscription", subscriptionSchema);
